78e782be0d30c0f8b278ce4f5ddf32bdc41fdb77
Booker Robinson <robinson.booker11@gmail.com>
2022-02-14 23:00:19 -0400

    send a bunch of test strings

--- sketch_feb14a/sketch_feb14a.ino
+++ sketch_feb14a/sketch_feb14a.ino
@@ -9,11 +9,52 @@ void send_bytes(String bytes){
 }
 
 
+
+
 void setup(){
  MyBlue.begin(9600);
 } 
 
 void loop(){
-  send_bytes("Hello World!\n");
+  send_bytes("Hey,\n");
   delay(500);
+  send_bytes("I\n");
+  delay(500);
+  send_bytes("Think\n");
+  delay(500);
+  send_bytes("It's\n");
+  delay(500);
+  send_bytes("Crazy\n");
+  delay(500);
+  send_bytes("That\n");
+  delay(500);
+  send_bytes("This\n");
+  delay(500);
+  send_bytes("Is\n");
+  delay(500);
+  send_bytes("Possible!\n");
+  delay(500);
+  send_bytes(".\n");
+  delay(500);
+  send_bytes(".\n");
+  delay(500);
+  send_bytes(".\n");
+  delay(500);
+  send_bytes(":)\n");
+  delay(1000);
 }
+
+/*
+
+def fib(n):
+    fib0 = 0
+    fib1 = 1
+    f  = 0
+    for i in range(n):
+        if i == 1:
+            continue
+        f = fib0 + fib1
+        fib0 = fib1
+        fib1 = f
+    return f
+*/
