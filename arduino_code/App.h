5a7dca3b51bbb1b784abbab7334761f434ff9621
Booker Robinson <robinson.booker11@gmail.com>
2022-02-27 14:04:29 -0400

    add Constructor and properties to App class

--- arduino_code/App.h
+++ arduino_code/App.h
@@ -1,8 +1,22 @@
-
-
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "Bluetooth.h"
+#include "JoyStick.h"
 
 class App{
-
+public:
+    String name;
+    Adafruit_SSD1306 *display;
+    JoyStick *joystick;
+    Bluetooth *bluetooth;
+    App(String name, Adafruit_SSD1306 *display,JoyStick *joystick, Bluetooth *bluetooth){
+        this->name = name;
+        this->display = display;
+        this->joystick = joystick;
+        this->bluetooth = bluetooth;
+    }
     virtual void init()   = 0;
     virtual void update() = 0;
     virtual void close()  = 0;
