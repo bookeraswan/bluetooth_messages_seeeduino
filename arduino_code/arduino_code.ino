735e136ba7aa990d24a6a5743387a207098da1f6
Booker Robinson <robinson.booker11@gmail.com>
2022-02-27 14:05:49 -0400

    comment out doubled includes

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -2,8 +2,9 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include "Bluetooth.h"
-#include "JoyStick.h"
+//#include "Bluetooth.h"
+//#include "JoyStick.h"
+#include "App.h"
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
